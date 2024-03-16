6f3b2bbb4e73479643a52c6bde1c01aaf864823d
Lina <biellyfernandes3@gmail.com>
2024-03-15 21:39:41 -0300

    add

--- atividade03/02.ts
+++ atividade03/02.ts
@@ -1,15 +1,10 @@
-function ehPrimo(numero: number): Boolean{
-    if (numero <= 1) {
+function par(numero: number){
+    if (numero % 2 === 0){
+            return true;
+            
+    } else {
         return false;
     }
-    for (let i = 2; i < numero ; i++){
-        if (numero % i === 0)
-            return false;
-            
-    } 
-        return true;
-    
 }
-const numero = 17;
-const resultado = ehPrimo(numero);
-console.log(`O numero ${numero} , é primo? ${resultado}`);
+
+console.log(par(4));
